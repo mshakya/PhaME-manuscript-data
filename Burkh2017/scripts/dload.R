f72791dc3d4d477164ab2c04db451c084bdf6d96
Migun Shakya <microbeatic@gmail.com>
2017-06-21 09:21:33 -0600

    more print functions

--- Burkh2017/scripts/dload.R
+++ Burkh2017/scripts/dload.R
@@ -19,28 +19,31 @@ store_path <- opt$output
 
 # setwd(store_path)
 folderz <- unlist(fread(opt$input, sep = "\t"))
-print(folderz)
-
 #
 userpwd <- "anonymous:anonymous"
 #
 for (folder in folderz) {
 
     path <- file.path(store_path, basename(folder))
-    dir.create(path = path, showWarnings = FALSE)
+    if (!dir.exists(path = path)){
+        dir.create(path = path, showWarnings = FALSE)
 
-    print(paste(Sys.time(), ": processing folder ", folder, " into ", path))
-    filenames <- getURL(folder, userpwd = userpwd,
+        print(paste(Sys.time(), ": processing folder ", folder, " into ", path))
+        filenames <- getURL(folder, userpwd = userpwd,
                     ftp.use.epsv = FALSE, dirlistonly = TRUE)
-    filenames <- strsplit(filenames, "\n", fixed = FALSE,
-        perl = FALSE, useBytes = FALSE)[[1]]
-      filenames <- filenames[grepl("*.gz", filenames)]
-      for (f in filenames) {
-        uri <- paste0(folder, f)
-        print(uri)
-        print(paste(Sys.time(), ": downloading file ",
-            f, " into ", file.path(path, f)))
-        curl_fetch_disk(uri, file.path(path, f))
+        print(filenames)
+        filenames <- strsplit(filenames, "\n", fixed = FALSE,
+            perl = FALSE, useBytes = FALSE)[[1]]
+        filenames <- filenames[grepl("*.gz", filenames)]
+        print(filenames)
+        for (f in filenames) {
+            uri <- paste0(folder, f)
+            print(uri)
+            print(paste(Sys.time(), ": downloading file ",
+                f, " into ", file.path(path, f)))
+            curl_fetch_disk(uri, file.path(path, f))
       }
+  }
+  else{print(paste(folder, " exists!, skipping!"))}
 
     }
